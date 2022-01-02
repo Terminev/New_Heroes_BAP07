74ea19571ca915223e8ac3ff471233bc48c3bf37
Alexandre <caramel.alexandre@gmail.com>
2022-01-02 15:45:00 +0100

    EDIT form registration for company

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('last_name');
+            $table->string('name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('localisation');
-            $table->integer('phone');
-            $table->integer('age');
+            $table->integer('phone')->unique();
+            $table->integer('age')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->string('company_name')->unique()->nullable();
+            $table->string('siret')->unique()->nullable();
+            $table->integer('number_employe')->nullable();
+            $table->string('name_representing')->nullable();
+            $table->string('industry')->nullable();
+            $table->boolean('entreprise')->nullable();
+
             $table->timestamps();
         });
     }
