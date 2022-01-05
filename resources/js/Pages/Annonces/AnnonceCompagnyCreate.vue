2aa45bbb8a40569f1c4e9580df479d0823040da1
QuentinLegreon <quentin.legron@edu.devinci.fr>
2022-01-05 12:48:20 +0100

    Beautify files Register and change English to french + add post element to AnnonceCompagnyCreate (the post doesn't post on the ddb for now)

--- resources/js/Pages/Annonces/AnnonceCompagnyCreate.vue
+++ resources/js/Pages/Annonces/AnnonceCompagnyCreate.vue
@@ -8,46 +8,116 @@
         </a>
 
         <div class="annonce-content">
-            <div>
-                <h3>Type de contrat :</h3>
-                <!-- <p>{{annonces.type_of_contract}}</p> -->
-            </div>
-            <div>
-                <h3>Domaine du contrat :</h3>
-                <!-- <p>{{annonces.domaine}}</p> -->
-            </div>
-            <div>
-                <h3>Service :</h3>
-                <!-- <p>{{annonces.announcement_title}}</p> -->
-            </div>
-            <div>
-                <h3>Temps de travail :</h3>
-                <!-- <p>{{annonces.time_work}}</p> -->
-            </div>
-            <div>
-                <h3>Nombre de post ouvert :</h3>
-                <!-- <p>Il reste {{annonces.number_of_jobs}} postes</p> -->
-            </div>
-            <div>
-                <h3>Rémuneration :</h3>
-                <!-- <p>{{annonces.pay}}€ / mois</p> -->
-            </div>
-            <div>
-                <!-- <p>{{annonces.announcement_description}}</p> -->
-            </div>
+            <form @submit.prevent="submit">
+
+                <div class="h-11 mt-8 mb-8">
+                    <h3>Service :</h3>
+                    <jet-input id="announcement_title" type="text" placeholder="Service" class="block w-full"
+                        v-model="form.announcement_title" required autofocus autocomplete="announcement_title" />
+                </div>
+
+                <div class="h-11 mt-8 mb-8">
+                    <h3>Type de contrat :</h3>
+                    <jet-input id="type_of_contract" type="text" placeholder="CDD/CDI" class="block w-full"
+                        v-model="form.type_of_contract" required autofocus autocomplete="type_of_contract" />
+                </div>
+
+                <div class="h-11 mt-8 mb-8">
+                    <h3>Domaine d'activité :</h3>
+                    <jet-input id="domaine" type="text" placeholder="Dans quelle domaine vous cherchez"
+                        class="block w-full" v-model="form.domaine" required autofocus autocomplete="domaine" />
+                </div>
+
+                <div class="h-11 mt-8 mb-8">
+                    <h3>Temps de travail :</h3>
+                    <jet-input id="time_work" type="number" placeholder="Combien d'heure par semaine"
+                        class="block w-full" v-model="form.time_work" required autofocus autocomplete="time_work" />
+                </div>
+
+                <div class="h-11 mt-8 mb-8">
+                    <h3>Nombre de post ouvert :</h3>
+                    <jet-input id="number_of_jobs" type="number" placeholder="nombre de post resant"
+                        class="block w-full" v-model="form.number_of_jobs" required autofocus
+                        autocomplete="number_of_jobs" />
+                </div>
+
+                <div class="h-11 mt-8 mb-8">
+                    <h3>Rémuneration :</h3>
+                    <jet-input id="pay" type="number" placeholder="Salaire/mois" class="block w-full" v-model="form.pay"
+                        required autofocus autocomplete="pay" />
+                </div>
+
+                <div class="h-11 mt-8 mb-8">
+                    <!-- Il faudra changer cette input pour mettre le truc html où on peut styliser directement dans le site  -->
+                    <h3>Déscription du post :</h3>
+                    <jet-input id="announcement_description" type="text"
+                        placeholder="Inscrivez ici tous ce qui vous passe par la tête" class="block w-full"
+                        v-model="form.announcement_description" required autofocus
+                        autocomplete="announcement_description" />
+                </div>
+
+                <div class="flex items-center justify-end">
+
+                    <jet-button class="w-full flex justify-center" :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing">
+                        CREER UNE ANNONCE
+                    </jet-button>
+
+                </div>
+            </form>
         </div>
 
     </div>
 </template>
 
 <script>
-    import Header from "@/Components/Header.vue";
+    import Header from '@/Components/Header.vue'
+    import {
+        defineComponent
+    } from 'vue'
+    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
+    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
+    import JetButton from '@/Jetstream/Button.vue'
+    import JetInput from '@/Jetstream/Input.vue'
+    import JetCheckbox from '@/Jetstream/Checkbox.vue'
+    import JetLabel from '@/Jetstream/Label.vue'
+    import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
 
-    export default {
+    export default defineComponent({
         components: {
             Header,
+            JetAuthenticationCard,
+            JetAuthenticationCardLogo,
+            JetButton,
+            JetInput,
+            JetCheckbox,
+            JetLabel,
+            JetValidationErrors,
         },
-    };
+
+        data() {
+            return {
+                form: this.$inertia.form({
+                    announcement_title: '',
+                    domaine: '',
+                    localisation: '',
+                    announcement_description: '',
+                    pay: '',
+                    type_of_contract: '',
+                    time_work: '',
+                    number_of_jobs: '',
+                }),
+            }
+        },
+
+        methods: {
+            submit() {
+                console.log(this.form)
+                this.$inertia.get(this.route('annonce_compagnie.create'), this.form)
+            }
+        }
+
+    })
 
 </script>
 
--- resources/js/Pages/Auth/RegisterCompany.vue
+++ resources/js/Pages/Auth/RegisterCompany.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <Head title="Register"/>
+    <Head title="Register" />
 
     <header>
         <img src="/storage/img/logo.png" alt="logo">
@@ -23,37 +23,42 @@
                 <div class="containeurimage">
                     <img class="age" src="/storage/img/age-group.png" alt="login">
                 </div>
-                <jet-input id="company_name" type="text" placeholder="Company_name" class="block w-full" v-model="form.company_name" required autocomplete="company_name" />
+                <jet-input id="company_name" type="text" placeholder="Company_name" class="block w-full"
+                    v-model="form.company_name" required autocomplete="company_name" />
             </div>
 
             <div class="border flex h-11 rounded">
                 <div class="containeurimage">
                     <img src="/storage/img/login2.png" alt="login">
                 </div>
-                <jet-input id="name_representing" type="text" placeholder="Name Representing"  class="block w-full" v-model="form.name_representing" required autofocus autocomplete="name" />
+                <jet-input id="name_representing" type="text" placeholder="Name Representing" class="block w-full"
+                    v-model="form.name_representing" required autofocus autocomplete="name" />
             </div>
 
             <div class="border flex h-11 rounded">
                 <div class="containeurimage">
                     <img src="/storage/img/login2.png" alt="login">
                 </div>
-                <jet-input id="siret" type="text" placeholder="Siret" class="block w-full" v-model="form.siret" required autofocus autocomplete="name" />
+                <jet-input id="siret" type="text" placeholder="Siret" class="block w-full" v-model="form.siret" required
+                    autofocus autocomplete="name" />
             </div>
 
 
 
-             <div class="border flex h-11 rounded">
-                 <div class="containeurimage">
+            <div class="border flex h-11 rounded">
+                <div class="containeurimage">
                     <img src="/storage/img/telephone.png" alt="login">
                 </div>
-                <jet-input id="phone" type="number" placeholder="Phone Number" class="block w-full" v-model="form.phone" required autofocus autocomplete="name" />
+                <jet-input id="phone" type="number" placeholder="Phone Number" class="block w-full" v-model="form.phone"
+                    required autofocus autocomplete="name" />
             </div>
 
             <div class="border flex h-11 rounded">
                 <div class="containeurimage">
                     <img src="/storage/img/@.png" alt="login">
                 </div>
-                <jet-input id="email" type="email" placeholder="E-mail" class="block w-full" v-model="form.email" required />
+                <jet-input id="email" type="email" placeholder="E-mail" class="block w-full" v-model="form.email"
+                    required />
             </div>
 
 
@@ -62,14 +67,16 @@
                 <div class="containeurimage">
                     <img src="/storage/img/lock.png" alt="login">
                 </div>
-                <jet-input id="password" type="password" placeholder="Password" class="block w-full" v-model="form.password" required autocomplete="new-password" />
+                <jet-input id="password" type="password" placeholder="Password" class="block w-full"
+                    v-model="form.password" required autocomplete="new-password" />
             </div>
 
             <div class="border flex h-11 rounded">
                 <div class="containeurimage">
                     <img src="/storage/img/lock.png" alt="login">
                 </div>
-                <jet-input id="password_confirmation" type="password" placeholder="Password Confirmation" class="block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+                <jet-input id="password_confirmation" type="password" placeholder="Password Confirmation"
+                    class="block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
@@ -78,19 +85,24 @@
                         <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
 
                         <div class="ml-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
+                            I agree to the <a target="_blank" :href="route('terms.show')"
+                                class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a
+                                target="_blank" :href="route('policy.show')"
+                                class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
                         </div>
                     </div>
                 </jet-label>
             </div>
 
             <div class="submit flex items-center justify-end flex-col-reverse">
-                <Link :href="route('login')" class="login underline text-sm text-black-900 hover:text-black-900">
-                    Already registered ?
+                <Link :href="route('login')"
+                    class="login text-center underline text-sm text-black-900 hover:text-black-900">
+                Vous avez déja un compte ? Connectez-vous
                 </Link>
 
-                <jet-button class="w-full flex justify-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    CREATE AN ACCOUNT
+                <jet-button class="w-full flex justify-center" :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing">
+                    CREER UN COMPTE
                 </jet-button>
             </div>
         </form>
@@ -100,7 +112,9 @@
 
 
 <script>
-    import { defineComponent } from 'vue'
+    import {
+        defineComponent
+    } from 'vue'
     import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
     import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
     import JetButton from '@/Jetstream/Button.vue'
@@ -108,7 +122,10 @@
     import JetCheckbox from '@/Jetstream/Checkbox.vue'
     import JetLabel from '@/Jetstream/Label.vue'
     import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
+    import {
+        Head,
+        Link
+    } from '@inertiajs/inertia-vue3';
 
     export default defineComponent({
         components: {
@@ -146,72 +163,67 @@
             }
         }
     })
+
 </script>
 
 
 <style scoped>
+    h1 {
+        font-weight: bold;
+        font-size: 30px;
+    }
 
-
-h1{
-    font-weight: bold;
-    font-size: 30px;
-}
-
-/* form{
+    /* form{
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 } */
 
-form{
-    padding: 30px 0;
-    padding-left: 50px;
-    padding-right: 50px;
-}
+    form {
+        padding: 30px 0;
+        padding-left: 50px;
+        padding-right: 50px;
+    }
 
-jet-input{
-    width: 257,68px;
-}
+    jet-input {
+        width: 257, 68px;
+    }
 
-.containeurimage{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 31px;
-    height: 100%;
-    background: linear-gradient(#bbd9db,#8aacd1);
-}
+    .containeurimage {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 31px;
+        height: 100%;
+        background: linear-gradient(#bbd9db, #8aacd1);
+    }
 
-.border{
-    margin-bottom: 15px;
-}
+    .border {
+        margin-bottom: 15px;
+    }
 
-.age{
-    width: 24px;
-    height: auto;
-}
+    .age {
+        width: 24px;
+        height: auto;
+    }
 
-.submit{
-    margin-top: 65px;
-}
+    .submit {
+        margin-top: 65px;
+    }
 
-.login{
-    padding-top:30px;
-}
+    .login {
+        padding-top: 30px;
+    }
 
-header{
-    width: 100%;
-    height: 63px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 15px;
-    padding-right: 15px;
-}
+    header {
+        width: 100%;
+        height: 63px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-left: 15px;
+        padding-right: 15px;
+    }
 
 </style>
-
-
-
-
