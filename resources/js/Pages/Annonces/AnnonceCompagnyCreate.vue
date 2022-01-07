718660d54e41cb3076e4899b85128252f9eeefce
QuentinLegreon <quentin.legron@edu.devinci.fr>
2022-01-07 14:33:03 +0100

    :lipstick: add style to AnnonceCompagnyCreate

--- resources/js/Pages/Annonces/AnnonceCompagnyCreate.vue
+++ resources/js/Pages/Annonces/AnnonceCompagnyCreate.vue
@@ -2,69 +2,68 @@
     <div>
         <Header> </Header>
 
-        <h1>Je creer une annonce</h1>
         <a :href="'/annonce/'" class="flex button-back">
             <img src="/storage/img/arrow_left.png" alt="favorite img" /> Retour
         </a>
+        <h1>Je creer une annonce</h1>
 
         <div class="annonce-content">
             <form @submit.prevent="submit">
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Service :</h3>
-                    <jet-input id="announcement_title" type="text" placeholder="Service" class="block w-full"
+                    <jet-input id="announcement_title" type="text" placeholder="Service" class="input block w-full"
                         v-model="form.announcement_title" required autofocus autocomplete="announcement_title" />
                 </div>
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Type de contrat :</h3>
-                    <jet-input id="type_of_contract" type="text" placeholder="CDD/CDI" class="block w-full"
+                    <jet-input id="type_of_contract" type="text" placeholder="CDD/CDI" class="input block w-full"
                         v-model="form.type_of_contract" required autofocus autocomplete="type_of_contract" />
                 </div>
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Où se situe votre entreprise:</h3>
-                    <jet-input id="localisation" type="text" placeholder="localisation" class="block w-full"
+                    <jet-input id="localisation" type="text" placeholder="localisation" class="input block w-full"
                         v-model="form.localisation" required autofocus autocomplete="localisation" />
                 </div>
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Domaine d'activité :</h3>
                     <jet-input id="domaine" type="text" placeholder="Dans quelle domaine vous cherchez"
-                        class="block w-full" v-model="form.domaine" required autofocus autocomplete="domaine" />
+                        class="input block w-full" v-model="form.domaine" required autofocus autocomplete="domaine" />
                 </div>
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Temps de travail :</h3>
                     <jet-input id="time_work" type="number" placeholder="Combien d'heure par semaine"
-                        class="block w-full" v-model="form.time_work" required autofocus autocomplete="time_work" />
+                        class="input block w-full" v-model="form.time_work" required autofocus autocomplete="time_work" />
                 </div>
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Nombre de post ouvert :</h3>
                     <jet-input id="number_of_jobs" type="number" placeholder="nombre de post resant"
-                        class="block w-full" v-model="form.number_of_jobs" required autofocus
+                        class="input block w-full" v-model="form.number_of_jobs" required autofocus
                         autocomplete="number_of_jobs" />
                 </div>
 
                 <div class="h-11 mt-8 mb-8">
                     <h3>Rémuneration :</h3>
-                    <jet-input id="pay" type="number" placeholder="Salaire/mois" class="block w-full" v-model="form.pay"
+                    <jet-input id="pay" type="number" placeholder="Salaire/mois" class="input block w-full" v-model="form.pay"
                         required autofocus autocomplete="pay" />
                 </div>
 
-                <div class="h-11 mt-8 mb-8">
-                    <!-- Il faudra changer cette input pour mettre le truc html où on peut styliser directement dans le site  -->
+                <div class="h-11 mt-8 mb-16">
                     <h3>Déscription du post :</h3>
-                    <jet-input id="announcement_description" type="textarea"
-                        placeholder="Inscrivez ici tous ce qui vous passe par la tête" class="block w-full"
+                    <textarea id="announcement_description"
+                        placeholder="Inscrivez ici tous ce qui vous passe par la tête" class="input block w-full"
                         v-model="form.announcement_description" required autofocus
                         autocomplete="announcement_description" />
-                </div>
+                    </div>
 
-                <div class="flex items-center justify-end">
+                <div class="flex items-center justify-center">
 
-                    <jet-button class="w-full flex justify-center" :class="{ 'opacity-25': form.processing }"
+                    <jet-button class="create-annonce-button w-full flex justify-center" :class="{ 'opacity-25': form.processing }"
                         :disabled="form.processing">
                         CREER UNE ANNONCE
                     </jet-button>
@@ -162,7 +161,7 @@
 
 <style scoped>
     h1 {
-        padding-top: 25px;
+        padding-top: 12px;
         padding-bottom: 16px;
         font-size: 24px;
         font-weight: bold;
@@ -171,11 +170,37 @@
 
     .button-back {
         color: #C9C9C9;
+        align-items: center;
+        justify-content: end;
+        padding-top: 12px;
+        padding-right: 12px;
     }
 
     .button-back img {
         object-fit: contain;
         padding-right: 4px;
+    }
+
+    .annonce-content {
+        padding: 4px 25px;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+
+    .input{
+        border: 1px solid black;
+    }
+
+    .create-annonce-button{
+        padding: 6px 43px;
+        margin-top: 24px;
+        margin-bottom: 10px;
+        font-size: 14px;
+        background-color: #001598;
+        color: #FFFFFF;
+        border-radius: 4px;
+        max-width: 250px;
     }
 
 </style>
